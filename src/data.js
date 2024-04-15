513c40331d8e520ad9b433f082dc8f11624d2978
backdfb <wva22lfr@gmail.com>
2024-04-15 22:12:16 +0900

    내용 추가 필요

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: '신상정보는?',
+    info: '김효중, 850128, 010-8072-6548, wva22lfr@gmail.com',
+  },
+  {
+    id: 2,
+    title: '스킬과 도구는?',
+    info: 'HTML, CSS, JavaScipt, React, MySql etc',
+  },
+  {
+    id: 3,
+    title: '현재직업은?',
+    info: '개발자로 취준',
+  },
+  {
+    id: 4,
+    title: 'Whose birth number can I use?',
+    info: 'Only You.',
+  },
+  {
+    id: 5,
+    title: 'When do I receive a password ordered by letter?',
+    info: 'Hmm I do not care.',
+  },
+];
+export default questions;
